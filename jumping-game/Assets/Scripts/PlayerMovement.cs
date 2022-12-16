cb405e594a7f691ed27f28010b1e141e0e6eb716
Xihai Luo <xihai.luo01@gmail.com>
2022-12-15 23:17:13 -0500

    gameplay design - add obstacles

--- jumping-game/Assets/Scripts/PlayerMovement.cs
+++ jumping-game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
     public float movementSpeed;
+    public float forwardForce;
     public bool leftKey = false;
     public bool rightKey = false;
     // Start is called before the first frame update
@@ -30,16 +31,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, -movementSpeed * Time.deltaTime);
+        rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
 
         if (leftKey)
         {
-            rb.AddForce(movementSpeed * Time.deltaTime, 0, 0);
+            rb.AddForce(movementSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
 
         if (rightKey)
         {
-            rb.AddForce(-movementSpeed * Time.deltaTime, 0, 0);
+            rb.AddForce(-movementSpeed * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
